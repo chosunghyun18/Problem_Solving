603b5968ac73a9301309b60b042a51ddbef00edf
chosunghyun18 <krismon@naver.com>
2022-05-11 19:21:38 +0900

    solution end of 200057

--- boj/simulation/200057.py
+++ boj/simulation/200057.py
@@ -1,61 +1,51 @@
-import pandas as pd
-
-
-
-def checkleft(x,y,visit):
-    if  visit[y][x] == 0:
-        return True
-    else :
-        return False
-
-
-def calculatesand(n,next_x,next_y,outsand,po,given):
-    print("this is x and y:",next_x,next_y)
-    print("this is po:",po)
+# import pandas as pd
+def calculatesand(n, next_x, next_y, outsand, po, given):
+    # print("this is x and y:",next_x,next_y)
+    # print("this is po:",po)
     # po 0 : <-     2:  ->         1 :down     3 : up
     # po 는 이전에서 온 진행 방향 , 좌표 주어진것들
     global dicratio
-
     q = next_y - 2
     p = next_x - 2
 
-    temp = 0
-    print("---")
-    print(dicratio[po][2][1])
-    print("---")
+    tmp = 0
     for j in range(5):
         for i in range(5):
-            if dicratio[po][j][i] != 'a' and dicratio[po][j][i] != 0:   # 비율 숫자
+            if dicratio[po][j][i] != 'a' and dicratio[po][j][i] != 0:  # 비율 숫자
                 # 상자 안
-                if 0 <= q + j <= n-1 and  0<= p + i <= n-1 :
-                        print(type(dicratio[po][j][i]))
-                        given[next_y][next_x] += given[next_y][next_x] * dicratio[po][j][i]// 100
+                if 0 <= q + j <= n - 1 and 0 <= p + i <= n - 1:
+                    given[q + j][p + i] += given[next_y][next_x] * dicratio[po][j][i] // 100
                 # 상자 밖으로
-                else :
+                else:
                     outsand += given[next_y][next_x] * dicratio[po][j][i] // 100
-                temp += given[next_y][next_x] * dicratio[po][j][i] // 100
+
+                tmp += given[next_y][next_x] * dicratio[po][j][i] // 100
+
             else:
-                if given[j][i] != 'a' :
-                    acord =(j,i)
-                else: continue
+                if dicratio[po][j][i] == 'a':
+                    acord = [j, i]
+                else: # 0
+                    continue
+
     # 나머지 a 부분 처리
-    if 0 <= acord[0] + q < n and 0 <= acord[1] + p < n:
-        given[acord[0]+q][acord[1]+p] += given[next_y][next_y] - temp
+    if 0 <= acord[0] + q <= n-1 and 0 <= acord[1] + p <= n-1:
+        given[acord[0]+q][acord[1]+p] += given[next_y][next_x] - tmp
     else:  # a 자리도 격자 바깥일 경우 격자 바깥으로 나가는 ans에 더해준다.
-        outsand += given[next_y][next_y] - temp
+        outsand += given[next_y][next_x] - tmp
 
-    return  outsand
+    given[next_y][next_x] = 0
+
+    return outsand, given
 
 
-def solv(n,given,visit):   # main  function to move
+def solv(n, given, visit):  # main  function to move
 
-    start = n //2
+    start = n // 2
     visit[start][start] = 1
 
     dirc_x = [-1, 0, 1, 0]
     dirc_y = [0, 1, 0, -1]
-    po =0
-
+    po = 0
 
     current_x = start
     current_y = start
@@ -64,63 +54,64 @@ def solv(n,given,visit):   # main  function to move
     for j in range(n):
         for i in range(n):
             totalsand += given[j][i]
-    print(totalsand)
+    # print(totalsand)
 
     outsand = 0
 
     next_x = current_x + dirc_x[po]
     next_y = current_y + dirc_y[po]
 
-
     while True:
-        if totalsand == outsand :
+
+        if totalsand == outsand:
             return outsand
 
-        if current_x == 1 and current_y == 0: #종료시점 바로 전
+        if current_x == 1 and current_y == 0:  # 종료시점 바로 전
             po -= 1
             next_x = current_x + dirc_x[po % 4]
             next_y = current_y + dirc_y[po % 4]
-            outsand += calculatesand(n,next_x, next_y,outsand,po % 4,given)
+            outsand, given = calculatesand(n, next_x, next_y, outsand, po % 4, given)
             return outsand
 
-        else :
+        else:
             if visit[next_y][next_x] == 0:  # 회전
-                outsand += calculatesand(n,next_x,next_y,outsand,po % 4,given)
-                current_x , current_y = next_x ,next_y
-                visit[current_y][current_x] = 1
+                outsand , given = calculatesand(n, next_x, next_y, outsand, po % 4, given)
 
-                po += 1
-                next_x = current_x + dirc_x[po%4]
-                next_y = current_y + dirc_y[po%4]
-
-
-
-            else :
-                po -= 1
-                next_x = current_x + dirc_x[po%4]
-                next_y = current_y + dirc_y[po%4]
-                outsand += calculatesand(n,next_x,next_y,outsand,po%4,given)
                 current_x, current_y = next_x, next_y
                 visit[current_y][current_x] = 1
 
                 po += 1
-                next_x = current_x + dirc_x[po%4]
-                next_y = current_y + dirc_y[po%4]
+                next_x = current_x + dirc_x[po % 4]
+                next_y = current_y + dirc_y[po % 4]
+
+
+
+            else:
+                po -= 1
+                next_x = current_x + dirc_x[po % 4]
+                next_y = current_y + dirc_y[po % 4]
+                outsand, given = calculatesand(n, next_x, next_y, outsand, po % 4, given)
+                current_x, current_y = next_x, next_y
+                visit[current_y][current_x] = 1
+
+                po += 1
+                next_x = current_x + dirc_x[po % 4]
+                next_y = current_y + dirc_y[po % 4]
 
 
 n = int(input())
-given =[ tuple(map(int,input().split()))
-            for _ in range(n)]
-visit = [  [0]*n for _ in range(n)]
+given = [list(map(int, input().split())) for _ in range(n)]
 
-dicratio ={
- 0: [[0, 0, 2, 0, 0], [0, 10, 7, 1, 0], [5, 'a', 0, 0, 0], [0, 10, 7, 1, 0], [0, 0, 2, 0, 0]],
- 1: [[0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [2, 7, 0, 7, 2], [0, 10, 'a', 10, 0], [0, 0, 5, 0, 0]],
- 2: [[0, 0, 2, 0, 0], [0, 1, 7, 10, 0], [0, 0, 0, 'a', 5], [0, 1, 7, 10, 0], [0, 0, 2, 0, 0]],
- 3:  [[0, 0, 5, 0, 0], [0, 10, 'a', 10, 0], [2, 7, 0, 7, 2], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0]]
+visit = [[0] * n for _ in range(n)]
+
+dicratio = {
+    0: [[0, 0, 2, 0, 0], [0, 10, 7, 1, 0], [5, 'a', 0, 0, 0], [0, 10, 7, 1, 0], [0, 0, 2, 0, 0]],
+    1: [[0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [2, 7, 0, 7, 2], [0, 10, 'a', 10, 0], [0, 0, 5, 0, 0]],
+    2: [[0, 0, 2, 0, 0], [0, 1, 7, 10, 0], [0, 0, 0, 'a', 5], [0, 1, 7, 10, 0], [0, 0, 2, 0, 0]],
+    3: [[0, 0, 5, 0, 0], [0, 10, 'a', 10, 0], [2, 7, 0, 7, 2], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0]]
 }
 
-solv(n,given,visit)
+print(solv(n, given, visit))
 
 
 
--- boj/tmp2.py
+++ boj/tmp2.py
@@ -1,53 +1,5 @@
 
 
-import pandas as  pd
-n = 5
-a =[[1,2,3,4,5],
-[1,2,3,4,5],
-[1,2,3,4,5],
-[1,2,3,4,5],
-[1,2,3,4,5]]
-
-
-print(pd.DataFrame(a ))
-
-
-# check  y 기준 있느지 ?
-
-
-#  index  check  star  in 0  and end <= n-1 1
-
-
-#     (  y  , x  )
-print(a[2][3])
-print(a[2][2])
-print(a[3][2])
-print(a[3][2])
-
-
-
-
-
-
-
-
-
-
-def func(a):
-    global dicratio
-    print(dicratio[a])
-    return 0
-
-
-
-
-
-
-dicratio = {
-    4: [[1]],
-    3: [[3]]
-}
-
-a = 3
-func
-
+a=1
+a+=1
+print(a)
