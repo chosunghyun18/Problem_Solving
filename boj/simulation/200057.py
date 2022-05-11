396b3bcfb90c4db508f8d3556ca2f38ffe5f25b9
chosunghyun18 <krismon@naver.com>
2022-05-11 13:03:00 +0900

    almost finish shark and tonado

--- boj/simulation/200057.py
+++ boj/simulation/200057.py
@@ -9,16 +9,45 @@ def checkleft(x,y,visit):
         return False
 
 
-def calculatesand(next_x,next_y,totalsand,outsand,po):
+def calculatesand(n,next_x,next_y,outsand,po,given):
     print("this is x and y:",next_x,next_y)
     print("this is po:",po)
     # po 0 : <-     2:  ->         1 :down     3 : up
+    # po 는 이전에서 온 진행 방향 , 좌표 주어진것들
+    global dicratio
+
+    q = next_y - 2
+    p = next_x - 2
+
+    temp = 0
+    print("---")
+    print(dicratio[po][2][1])
+    print("---")
+    for j in range(5):
+        for i in range(5):
+            if dicratio[po][j][i] != 'a' and dicratio[po][j][i] != 0:   # 비율 숫자
+                # 상자 안
+                if 0 <= q + j <= n-1 and  0<= p + i <= n-1 :
+                        print(type(dicratio[po][j][i]))
+                        given[next_y][next_x] += given[next_y][next_x] * dicratio[po][j][i]// 100
+                # 상자 밖으로
+                else :
+                    outsand += given[next_y][next_x] * dicratio[po][j][i] // 100
+                temp += given[next_y][next_x] * dicratio[po][j][i] // 100
+            else:
+                if given[j][i] != 'a' :
+                    acord =(j,i)
+                else: continue
+    # 나머지 a 부분 처리
+    if 0 <= acord[0] + q < n and 0 <= acord[1] + p < n:
+        given[acord[0]+q][acord[1]+p] += given[next_y][next_y] - temp
+    else:  # a 자리도 격자 바깥일 경우 격자 바깥으로 나가는 ans에 더해준다.
+        outsand += given[next_y][next_y] - temp
+
+    return  outsand
 
 
-
-    return totalsand , outsand
-
-def solv(n,given,visit):
+def solv(n,given,visit):   # main  function to move
 
     start = n //2
     visit[start][start] = 1
@@ -51,12 +80,12 @@ def solv(n,given,visit):
             po -= 1
             next_x = current_x + dirc_x[po % 4]
             next_y = current_y + dirc_y[po % 4]
-            totalsand, outsand = calculatesand(next_x, next_y, totalsand, outsand,po % 4)
+            outsand += calculatesand(n,next_x, next_y,outsand,po % 4,given)
             return outsand
 
         else :
             if visit[next_y][next_x] == 0:  # 회전
-                totalsand, outsand = calculatesand(next_x,next_y, totalsand, outsand,po % 4)
+                outsand += calculatesand(n,next_x,next_y,outsand,po % 4,given)
                 current_x , current_y = next_x ,next_y
                 visit[current_y][current_x] = 1
 
@@ -70,11 +99,9 @@ def solv(n,given,visit):
                 po -= 1
                 next_x = current_x + dirc_x[po%4]
                 next_y = current_y + dirc_y[po%4]
-                totalsand, outsand = calculatesand(next_x,next_y, totalsand, outsand,po%4)
-
+                outsand += calculatesand(n,next_x,next_y,outsand,po%4,given)
                 current_x, current_y = next_x, next_y
                 visit[current_y][current_x] = 1
-
 
                 po += 1
                 next_x = current_x + dirc_x[po%4]
@@ -86,6 +113,12 @@ given =[ tuple(map(int,input().split()))
             for _ in range(n)]
 visit = [  [0]*n for _ in range(n)]
 
+dicratio ={
+ 0: [[0, 0, 2, 0, 0], [0, 10, 7, 1, 0], [5, 'a', 0, 0, 0], [0, 10, 7, 1, 0], [0, 0, 2, 0, 0]],
+ 1: [[0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [2, 7, 0, 7, 2], [0, 10, 'a', 10, 0], [0, 0, 5, 0, 0]],
+ 2: [[0, 0, 2, 0, 0], [0, 1, 7, 10, 0], [0, 0, 0, 'a', 5], [0, 1, 7, 10, 0], [0, 0, 2, 0, 0]],
+ 3:  [[0, 0, 5, 0, 0], [0, 10, 'a', 10, 0], [2, 7, 0, 7, 2], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0]]
+}
 
 solv(n,given,visit)
 
--- boj/tmp2.py
+++ boj/tmp2.py
@@ -1,5 +1,28 @@
 
 
+import pandas as  pd
+n = 5
+a =[[1,2,3,4,5],
+[1,2,3,4,5],
+[1,2,3,4,5],
+[1,2,3,4,5],
+[1,2,3,4,5]]
+
+
+print(pd.DataFrame(a ))
+
+
+# check  y 기준 있느지 ?
+
+
+#  index  check  star  in 0  and end <= n-1 1
+
+
+#     (  y  , x  )
+print(a[2][3])
+print(a[2][2])
+print(a[3][2])
+print(a[3][2])
 
 
 
@@ -9,8 +32,22 @@
 
 
 
-print(0%4)
-print(1%4)
-print(2%4)
-print(3%4)
-print(0,1,2,3)
+
+def func(a):
+    global dicratio
+    print(dicratio[a])
+    return 0
+
+
+
+
+
+
+dicratio = {
+    4: [[1]],
+    3: [[3]]
+}
+
+a = 3
+func
+
