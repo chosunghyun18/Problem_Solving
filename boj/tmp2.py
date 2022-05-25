246ef299c3354e0aa3aeb139cef911b79bacac08
chosunghyun18 <krismon@naver.com>
2022-05-25 09:45:27 +0900

    add 1987 ans

--- boj/tmp2.py
+++ boj/tmp2.py
@@ -1,23 +1,23 @@
 
 
 
-n = int(input())
-visit = [0] * n
 
-graph = [[] for _ in range(n)]
 
-start ,end =  map(int , input().split())
 
-m = int(input())
-
-for _ in range(m):
-    x,y = map(int , input().split())
-    graph[x-1].append(y-1)
+a= "fdfdf"
+print(len(a))
 
 
 
 
-print(graph)
+b = a.replace(""," ").split()
+print(b)
+
+if "f" in b:
+    print("hello")
+
+
+
 
 
 
