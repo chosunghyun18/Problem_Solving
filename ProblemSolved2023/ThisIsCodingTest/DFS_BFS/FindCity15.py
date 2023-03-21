2641ba8d0eea13f91e26eb679aeffbe52bc9b943
chosunghyun18 <krismon@naver.com>
2023-03-21 23:37:46 +0900

    feat : add the solution of the FindCity - deque 활용이 부족했다. - distance 를 활용하여 현제 상태를 나타냄

--- ProblemSolved2023/ThisIsCodingTest/DFS_BFS/FindCity15.py
+++ ProblemSolved2023/ThisIsCodingTest/DFS_BFS/FindCity15.py
@@ -54,3 +54,47 @@
 #
 # 구현이 너무 오래 걸림 , count 맞추는데 시간 다씀 .
 
+
+
+
+# #도시 , 도로 , 거리 정보, 출발 도시
+from collections import  deque
+n,m,k,x = map(int,input().split())
+
+# graph
+graph = [[] for _ in range(n+1)]
+
+# get all road info ,
+for _ in range(m) :
+    a,b = map(int,input().split())
+    graph[a].append(b)
+
+
+# 모든 도시 최단 거리 초기화
+distance = [-1]*(n+1)
+distance[x] = 0 # 출발도시까지 거리는 0 으로 설정
+# distance 배열을 적극적으로 사용한다.
+
+
+q = deque([x])
+while q:
+    now = q.popleft()
+    for next_node in graph[now]:
+        if distance[next_node] == -1 :
+            distance[next_node] =distance[now] + 1
+            q.append(next_node)
+
+# 최단 거리가 k 인 모든 도시의 번호를 오름 차순으로 출력
+for i in range(1,n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
+
+if check == False :
+    print(-1)
+
+
+
+
+
+
