a68b61ae9392dd910c3cdacd2540bc55b77d592c
chosunghyun18 <krismon@naver.com>
2023-04-04 15:34:14 +0900

    feat : add the soultion of route 29

--- ProblemSolved2023/ThisIsCodingTest/search/Router29.py
+++ ProblemSolved2023/ThisIsCodingTest/search/Router29.py
@@ -14,7 +14,7 @@ visited = [False] * n
 
 start = 0
 end = n
-mid = 0
+
 
 
 def binary(arr,start,end) :
@@ -26,6 +26,44 @@ def binary(arr,start,end) :
         #condition of chek right
         mid
         return binary(arr,mid+1,end)
+
+
+
+n,c = list(map(int,input().split()))
+array = []
+for _ in range(n):
+    array.append(int(input()))
+array.sort()
+
+start =1
+end = array[-1] - array[0]
+result = 0
+
+while (start<=end) :
+    mid = end+start // 2  # 가장 인접한 두 공유기의 사이의 거리를 의미
+    value = array[0]
+    count = 1
+    # install router
+    for i in range(1,n) :
+        if array[i] >= value + mid :
+            value = array[i]
+            count += 1
+        if count >= c :  # 추가적인 공유기 설치가 가능하면 거리를 증가 시킨다.
+            start = mid + 1
+            result = mid
+        else :
+            end =  mid -1
+
+print(result)
+
+
+
+
+
+
+
+
+
 
 
 
