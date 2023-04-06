c2098d15b447fa4d4fc217cb836d016358bed6a4
chosunghyun18 <krismon@naver.com>
2023-04-06 14:24:57 +0900

    chore : change name i into node

--- ProblemSolved2023/ThisIsCodingTest/EnhancedDikstra.py
+++ ProblemSolved2023/ThisIsCodingTest/EnhancedDikstra.py
@@ -1,61 +1,35 @@
 import heapq
-"""
-Ready to get input call import
-"""
-import sys
-input = sys.stdin.readline
-INF = int(1e9) # 10 억
+import  sys
 
-"""
-Ready to get input
-"""
-# n = node ,. m = edge
-n, m = map(int,input().split())
+input= sys.stdin.readline
 
+n, m = map(int, input().split())
 start = int(input())
 
 
-arr = [[] for _ in range( n+1 )] # 그래프
-d = [INF] * (n+1)
+graph = [[] for _ in range(n+1)]
 
-d[start] = 0  # init starting point
+for _ in range(m):
+    a,b,c  = map(int, input().split())
+    graph[a].append((b,c))
 
-#get edge
-for _ in range(m) :
-    a,b,c = map(int,input().split())
-    arr[a].append((b,c))
+INF = int(1e9)
+distance = [INF]*(n+1)
 
-
-
-"""
-Define function dijkstra
-"""
+distance[start] = 0
 
 def f(start):
     q = []
-    heapq.heappush(q,(0,start))  # [(0, 1)]
-    print("heapq:",q)
+    heapq.heappush(q,(0,start))
     while q :
         dist , now = heapq.heappop(q)
-        print("dist :",dist,"now : ",now)
-        # if current node is already vosited just skip
-        if d[now] < dist :
+        if dist > distance[now] :
             continue
-        # check near node in current node
-        for i in arr[now] :
-            cost = dist+ i[1]
-            # 현제 노드를 거쳐서 , 다른 노드로 이동하는 거리가 더 짧은 경우
-            if  cost < d[i[0]] :
-                d[i[0]] = cost
-                heapq.heappush(q,(cost,i[0]))
-                print("heapq:", q)
-
-
-
-
-    return start
-
-
+        for node in graph[now] :
+            cost = dist + node[1]
+            if cost < distance[node[0]]:
+                distance[node[0]] = cost
+                heapq.heappush(q,(cost,node[0]))
 
 
 """
@@ -66,11 +40,13 @@ call result
 
 f(start)
 print("--Output :--")
-for i in range(1,n+1) :
-    if d[i] == INF  :
-        print("can't go")
+for i in range(1,n+1):
+    if distance[i] == INF :
+        print("no")
+
     else:
-        print(d[i])
+        print(distance[i])
+
 
 
 
@@ -80,7 +56,7 @@ Test case
 6 11
 1
 1 2 2
-1 3 5
+1 3 5git 
 1 4 1
 2 3 3
 2 4 2
@@ -93,13 +69,3 @@ Test case
 
 
 """
-
-
-
-
-
-
-
-
-
-
