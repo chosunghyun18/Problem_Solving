343155df8cf2ebb29f9ef32ac8ca38e6b6af5cdf
chosunghyun18 <krismon@naver.com>
2023-04-10 15:16:01 +0900

    feat : add solution

--- ProblemSolved2023/boj/bfs/WarmUP.py
+++ ProblemSolved2023/boj/bfs/WarmUP.py
@@ -37,49 +37,44 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
+
 n,m = map(int,input().split())
-print(n,m)
 
-visited = [[0]*m for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
 graph = [input().replace(""," ").split() for _ in range(n)]
-
 start = (0,0)
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
 
 def bfs(graph,start,visited):
-    count = 0
     q =deque([start])
     while q :
         now = q.popleft()
         x = now[0]
         y = now[1]
-        if graph[x][y] == '#' and visited[now[0]][now[1]] == 0:
-            count +=1
-            for i in range(4):
-                x += dx[i]
-                y += dy[i]
-                if x < 0 or y < 0 or x > n or y> n :
-                    continue
-                if graph[x][y] == '#' and visited[x][y] == 0:
-                    visited[x][y] = 1
-                    q.append((x,y))
-    return count
+        visited[x][y] = True
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny>= m :
+                continue
+            if graph[nx][ny] == '#' and visited[nx][ny] == False:
+                graph[nx][ny] = "1"
+                visited[nx][ny] = True
+                q.append((nx,ny))
+
+    return graph , visited
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == '#' and not visited[i][j]:
+            graph , visited =bfs(graph, (i,j), visited)
+            ans+=1
 
 
-
-def solution(data):
-    ans = 0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == '#' :
-                ans += bfs(graph, start, visited)
-
-
-
-
-
+print(ans)
 
 
 
