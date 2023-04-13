be9e5c90c76127c79b4298c841d04266656b1041
chosunghyun18 <krismon@naver.com>
2023-04-13 13:47:57 +0900

    feat : add sol all n and m - 유형 별 풀이 중요

--- ProblemSolved2023/boj/backTracking/NM15656.py
+++ ProblemSolved2023/boj/backTracking/NM15656.py
@@ -6,27 +6,21 @@ n,m = map(int,input().split())
 arr = list(map(int,input().split()))
 arr.sort()
 
-
-visited = [False] * n
 result = []
-
 def dfs(n,m):
     if len(result) == m :
         for item  in result:
             print(item,end=" ")
         print()
+        return
 
-    for i in range(n):
-        if visited[i] == True:
-            continue
-        if len(result) != 0 :
-            if result[-1] > arr[i]:
-                continue
-        visited[i] = True
-        result.append(arr[i])
+    for node in arr :
+        result.append(node)
         dfs(n,m)
-        visited[i] = False
         result.pop()
+
+
+
 
 
 
--- ProblemSolved2023/boj/backTracking/NM15657.py
+++ ProblemSolved2023/boj/backTracking/NM15657.py
@@ -1,23 +1,30 @@
 import sys
+
 input = sys.stdin.readline
-
-
 n,m = map(int,input().split())
-result =[]
-visited= [False]*(n+1)
-def dfs(n,m) :
-    if m == len(result) :
-        for item in result:
-            print(item,end=' ')
+
+arr = list(map(int,input().split()))
+arr.sort()
+
+result = []
+def dfs(n,m):
+    if len(result) == m :
+        for item  in result:
+            print(item,end=" ")
         print()
+        return
 
-    for i in range(1,n+1):
-        if not visited[i] :
-            visited[i] = True
-            result.append(i)
-            dfs(n,m)
-            result.pop()
-            visited[i] = False
+    for i in range(n) :
+        if len(result) != 0:
+            if result[-1] > arr[i] :
+                continue
+        result.append(arr[i])
+        dfs(n,m)
+        result.pop()
 
 
-dfs(n,m)
+
+
+
+
+dfs(n, m)
