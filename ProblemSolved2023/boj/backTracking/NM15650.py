3e0fd0d6fc81a5b6b3fe2fe63e15a7933ccd9877
chosunghyun18 <krismon@naver.com>
2023-04-13 13:14:22 +0900

    feat: add sol n and m 2

--- ProblemSolved2023/boj/backTracking/NM15650.py
+++ ProblemSolved2023/boj/backTracking/NM15650.py
@@ -1,3 +1,26 @@
 import sys
 input = sys.stdin.readline
+
+
 n,m = map(int,input().split())
+result =[]
+visited= [False]*(n+1)
+def dfs(n,m) :
+    if m == len(result) :
+        for item in result:
+            print(item,end=' ')
+        print()
+
+    for i in range(1,n+1):
+        if len(result) !=0 :
+            if result[-1] > i :
+                continue
+        if not visited[i] :
+            visited[i] = True
+            result.append(i)
+            dfs(n,m)
+            result.pop()
+            visited[i] = False
+
+
+dfs(n,m)
