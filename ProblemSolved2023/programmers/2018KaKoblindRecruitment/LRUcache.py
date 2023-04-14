366f8cee8606ac536692c3adc4a06296a60c6b49
chosunghyun18 <krismon@naver.com>
2023-04-14 16:37:42 +0900

    kako blind 구현 problem and solving

---
+++ ProblemSolved2023/programmers/2018KaKoblindRecruitment/LRUcache.py
@@ -0,0 +1,14 @@
+# # LRU : Least Recently used algo
+#
+#
+# def cal_cache(cache,string ,cache_size):
+#     try:
+#         if len(cache) > cache_size:
+#             if cache.count(string)
+#
+#
+#
+#
+#
+#
+#
