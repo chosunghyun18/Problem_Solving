f043f97033583e4ed19f3823de1d64e420145d1f
chosunghyun18 <krismon@naver.com>
2023-04-14 18:51:10 +0900

    feat: add solution

---
+++ ProblemSolved2023/programmers/2021kakoreplace.py
@@ -0,0 +1,7 @@
+def solution(s):
+    answer = 0
+    a = ['zero', 'one', 'two', 'three', 'four',
+         'five', 'six', 'seven', 'eight', 'nine']
+    for i in a:
+        s = s.replace(i, str(a.index(i)))
+    return int(s)
