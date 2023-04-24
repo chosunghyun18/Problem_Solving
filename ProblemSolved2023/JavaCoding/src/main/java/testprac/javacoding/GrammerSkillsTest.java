68ab78b0bce88146c55428927d57ab5afe6c5648
chosunghyun18 <krismon@naver.com>
2023-04-24 16:54:24 +0900

    feat : add java grammer dijkstra and simple list handle

--- ProblemSolved2023/JavaCoding/src/main/java/testprac/javacoding/GrammerSkillsTest.java
+++ ProblemSolved2023/JavaCoding/src/main/java/testprac/javacoding/GrammerSkillsTest.java
@@ -1,33 +1,36 @@
 package testprac.javacoding;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import java.util.*;
 
 public class GrammerSkillsTest {
 
     @Test
-    public void arrayListTest(){
-        ArrayList<Integer> givenList = new ArrayList<>(Arrays.asList(3,2,1));
+    public void arrayListTest() {
+        ArrayList<Integer> givenList = new ArrayList<>(Arrays.asList(3, 2, 1));
         givenList.stream().forEach(System.out::println);
     }
+
     @Test
-    public void splitTest(){
+    public void splitTest() {
         int value = 123;
-        while(value != 0){
-            System.out.println(value%10);
-            value /= 10 ;
+        while (value != 0) {
+            System.out.println(value % 10);
+            value /= 10;
 
         }
     }
+
     @Test
-    public void listTest(){
-        int[] items = {1,2,3,4};
+    public void listTest() {
+        int[] items = {1, 2, 3, 4};
         ArrayList<Integer> checkList = new ArrayList<>();
-        for(int item : items){
-           checkList.add(item) ;
+        for (int item : items) {
+            checkList.add(item);
         }
         readList(checkList);
 
-        ArrayList<Integer> givenList = new ArrayList<>(Arrays.asList(1,2,3));
+        ArrayList<Integer> givenList = new ArrayList<>(Arrays.asList(1, 2, 3));
         List arrayList = new ArrayList<>(givenList);
         List linkedList = new LinkedList(givenList);
 
@@ -36,10 +39,103 @@ public class GrammerSkillsTest {
         System.out.println("");
         readList(linkedList);
     }
-    private void readList(List anyList){
+
+    private void readList(List anyList) {
         anyList.stream().forEach(System.out::println);
     }
+
+    @Test
+    public void list_handle_Test() {
+        int[] items = {1, 2, 3, 4, 5, 6};
+        ArrayList<Integer> arrlist = new ArrayList<>();
+        for (int item : items) {
+            arrlist.add(item);
+        }
+        for (int i = 0; i < items.length; i++) {
+            arrlist.add(items[i]);
+        }
+
+    }
+
+    @Test
+    public void String_handle_test() {
+        String numbers = "789123";
+        String[] number = numbers.split("");
+        for (String n : number) {
+            System.out.println(n);
+        }
+    }
+
+    @Test
+    public void priority_queue() {
+        PriorityQueue<Integer> priorityQueueLower = new PriorityQueue<>();
+        PriorityQueue<Integer> priorityQueueUpper = new PriorityQueue<>(Collections.reverseOrder());
+
+        priorityQueueLower.add(10);
+        priorityQueueLower.add(20);
+        priorityQueueLower.add(15);
+        priorityQueueLower.add(15);
+        priorityQueueLower.add(40);
+
+
+    }
+
+    static class Node {
+        public int node;
+        public int cost;
+
+        public Node(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+    @Test
+    public int[] dijkstra() {
+        List<List<Node>> graph = new ArrayList<>();
+        // src ref start point node
+        int src = 0;
+        int n = graph.size();
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>(n, Comparator.comparingInt(node -> node.cost));
+        pq.offer(new Node(src, 0));
+
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int u = node.node;
+            int cost = node.cost;
+
+            if (cost != dist[u]) {
+                continue;
+            }
+
+            for (Node neighbor : graph.get(u)) {
+                int alt = dist[u] + neighbor.cost;
+                if (alt < dist[neighbor.node]) {
+                    dist[neighbor.node] = alt;
+                    pq.offer(new Node(neighbor.node, alt));
+                }
+            }
+        }
+
+        return dist;
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
