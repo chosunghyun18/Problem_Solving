d4617c69280096822b5498cb45079fc60f82ed3d
chosunghyun18 <krismon@naver.com>
2023-07-07 14:38:38 +0900

    .

--- ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0707.java
+++ ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0707.java
@@ -7,16 +7,40 @@ import org.junit.jupiter.api.Test;
 
 public class pg0707 {
     @Test
-    void sol(){
+    void sol1(){
 //     String given =  "dxccxbbbxcxabaaxaaba" ;
         String given = "a";
-      solution(given);
+      solution1(given);
     }
-    public List<String> solution(String myString) {
+    public List<String> solution1(String myString) {
         //    //13:48 ~ 13:50
         return Arrays.stream(myString.split("x"))
             .filter(str -> !str.isEmpty())
             .sorted().collect(Collectors.toList()) ;
     }
+    @Test
+    void sol2(){
+        int given = 1081 ;
+        solution(given);
+    }
+    public int solution(int chicken) {
+        //14:24 ~
+        if(chicken < 10) return 0 ;
+        int service = 0 ;
+        int coupon = chicken ;
+        while(chicken > 9 ){
+            int left = 0 ;
+            int serviceTmp = 0 ;
+            serviceTmp += chicken / 10 ;
+            service += serviceTmp ;
+            left = chicken % 10;
+            chicken = serviceTmp + left;
+        }
+
+        return service;
+    }
+
+
+
 
 }
