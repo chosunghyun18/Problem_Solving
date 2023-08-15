73c41cf9c72f5a4a00eea4be380489083ff867ec
chosunghyun18 <krismon@naver.com>
2023-08-15 21:38:25 +0900

    feat : add two sum with one loop using two pointer

--- ProblemSolved2023/JavaCoding/src/test/java/testprac/leetcode/lg0811.java
+++ ProblemSolved2023/JavaCoding/src/test/java/testprac/leetcode/lg0811.java
@@ -30,7 +30,23 @@ public class lg0811 {
         }
         return true ;
     }
+    @Test
+    void two_pointer_two_sum_test(){
+        int[] numbers = {2,7,11,15} ;
+        int t = 9 ;
+        int[] re = twoSum(numbers,t);
 
+    }
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0 , right = numbers.length - 1;
+        while (left < right){
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) return new int[]{left+1,right+1};
+            if(sum > target) right--;
+            if(sum < target) left++;
+        }
+        return new int[]{};
+    }
 }
 
 
