1f9f057e9aa07ff10210f712796a334a58a0564d
chosunghyun18 <krismon@naver.com>
2023-09-03 18:34:39 +0900

    feat : add solution - 효율성 안좋음

--- ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0903.java
+++ ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0903.java
@@ -2,28 +2,39 @@ package testprac.programmers;
 
 import java.util.*;
 import java.util.stream.*;
+import org.junit.jupiter.api.Test;
 
 
 public class pg0903 {
+    @Test
+    public void test(){
+        //int[] s = {1, 2, 3, 9, 10, 12} ;
+        int[] s = {1, 1,1,10} ;
+        int k = 9;
+        System.out.println(solution(s, k));
+    }
     int target = 0;
     int[] scoville ;
     public int solution(int[] scoville, int K) {
         target = K;
-        this.scoville = scoville;
+        this.scoville = scoville.clone();
         int answer = 0;
         Arrays.sort(this.scoville);
         boolean done = false ;
         while(!done) {
             if(!isDone()){
                 answer++;
-                int value = scoville[0]+(scoville[1]*2);
-                int[] tmp = Arrays.copyOfRange(scoville,1,scoville.length);
+                if(this.scoville.length <= 1) {
+                    if(this.scoville[0] < target) return -1;
+                    return answer;
+                }
+                int value = this.scoville[0]+(this.scoville[1]*2);
+                int[] tmp = Arrays.copyOfRange(this.scoville,1,this.scoville.length);
                 tmp[0] = value;
                 System.out.println(Arrays.toString(tmp));
                 this.scoville = tmp.clone();
                 Arrays.sort(this.scoville);
             }else{
-                done = true;
                 return answer;
             }
         }
