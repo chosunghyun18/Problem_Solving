8f57877b2d88d4686c18c4b943d3ae54a8ff2cf0
chosunghyun18 <krismon@naver.com>
2023-09-04 21:48:33 +0900

    feat : add bubble and insertion  sort

--- ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/fall/pg0904.java
+++ ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/fall/pg0904.java
@@ -52,6 +52,53 @@ public class pg0904 {
             });
             System.out.println(arr);
         }
+        @Test
+        void sorting_test_for_java(){
+            List<Integer> arr = new ArrayList<>();
+            arr.add(1);
+            arr.add(5);
+            arr.add(1);
+            arr.add(5);
+            arr.add(18);
+            arr.add(8);
+            bubble_sorting_test(arr);    // change each nearby  : make max or min to push all in one
+            selection_sorting_test(arr);  // find max or min to sort it
+        }
+
+    private void selection_sorting_test(List<Integer> arr) {
+
+            for(int j = 0; j <arr.size() -1 ; j++){
+                int max = Integer.MIN_VALUE;
+                int index = arr.size()+1 ;
+                for(int i = 0 ; i < arr.size()-1 - j ; i++ ) {
+                        int check = arr.get(0);
+                        if(max  < check){
+                            max = check;
+                            index = i ;
+                        }
+                }
+                arr.set(arr.size()-1 ,arr.get(index));
+            }
+
+            System.out.println("selecction : "+arr);
+    }
+
+    // tmp , tmp, tmp , 마지막에 큰거 넣기
+    public void bubble_sorting_test(List<Integer> arr) {
+            System.out.println(arr);
+            for(int j = 0 ; j < arr.size() - 1 ; j++){
+                for(int i =0 ; i <arr.size()-1 - j;i++){
+                    if(arr.get(i) < arr.get(i+1)){
+                        int tmp = arr.get(i+1);
+                        arr.set((i+1) ,  arr.get(i) ) ;
+                        arr.set((i) ,  tmp) ;
+                    }
+                }
+            }
+        System.out.println(arr);
+    }
+
+
 
 
 }
