a416814ec8c0838392e290dcb00072896785ca86
chosunghyun18 <krismon@naver.com>
2023-09-04 13:32:41 +0900

    feat  : add solution

--- ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0903.java
+++ ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0903.java
@@ -11,11 +11,11 @@ public class pg0903 {
         //int[] s = {1, 2, 3, 9, 10, 12} ;
         int[] s = {1, 1,1,10} ;
         int k = 9;
-        System.out.println(solution(s, k));
+        System.out.println(solution1(s, k));
     }
     int target = 0;
     int[] scoville ;
-    public int solution(int[] scoville, int K) {
+    public int solution1(int[] scoville, int K) {
         target = K;
         this.scoville = scoville.clone();
         int answer = 0;
@@ -49,6 +49,35 @@ public class pg0903 {
             }
         }
         return true;
+    }
+    @Test
+    public void sol_test(){
+        int[] p = {1,2,7,2,3,8,6,1};
+        System.out.println(Arrays.stream(solution(p)));
+    }
+
+
+
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+
+            stack.push(i);
+        }
+
+
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = prices.length - idx - 1;
+        }
+
+        return answer;
     }
 
 }
