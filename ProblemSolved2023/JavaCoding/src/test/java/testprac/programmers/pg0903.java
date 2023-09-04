5f1c76f8f2a01c7be13bb2fb5d5109bfd1eb36ee
chosunghyun18 <krismon@naver.com>
2023-09-04 15:15:46 +0900

    refact : remove continue logic

--- ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0903.java
+++ ProblemSolved2023/JavaCoding/src/test/java/testprac/programmers/pg0903.java
@@ -6,56 +6,62 @@ import org.junit.jupiter.api.Test;
 
 
 public class pg0903 {
+
     @Test
-    public void test(){
+    public void test() {
         //int[] s = {1, 2, 3, 9, 10, 12} ;
-        int[] s = {1, 1,1,10} ;
+        int[] s = {1, 1, 1, 10};
         int k = 9;
         System.out.println(solution1(s, k));
     }
+
     int target = 0;
-    int[] scoville ;
+    int[] scoville;
+
     public int solution1(int[] scoville, int K) {
         target = K;
         this.scoville = scoville.clone();
         int answer = 0;
         Arrays.sort(this.scoville);
-        boolean done = false ;
-        while(!done) {
-            if(!isDone()){
+        boolean done = false;
+        while (!done) {
+            if (!isDone()) {
                 answer++;
-                if(this.scoville.length <= 1) {
-                    if(this.scoville[0] < target) return -1;
+                if (this.scoville.length <= 1) {
+                    if (this.scoville[0] < target) {
+                        return -1;
+                    }
                     return answer;
                 }
-                int value = this.scoville[0]+(this.scoville[1]*2);
-                int[] tmp = Arrays.copyOfRange(this.scoville,1,this.scoville.length);
+                int value = this.scoville[0] + (this.scoville[1] * 2);
+                int[] tmp = Arrays.copyOfRange(this.scoville, 1, this.scoville.length);
                 tmp[0] = value;
                 System.out.println(Arrays.toString(tmp));
                 this.scoville = tmp.clone();
                 Arrays.sort(this.scoville);
-            }else{
+            } else {
                 return answer;
             }
         }
 
         return -1;
     }
-    public boolean isDone(){
-        for(int i = 0 ; i<scoville.length;i++) {
+
+    public boolean isDone() {
+        for (int i = 0; i < scoville.length; i++) {
             int check = scoville[i];
-            if(check < target) {
+            if (check < target) {
                 return false;
             }
         }
         return true;
     }
+
     @Test
-    public void sol_test(){
-        int[] p = {1,2,7,2,3,8,6,1};
+    public void sol_test() {
+        int[] p = {1, 2, 7, 2, 3, 8, 6, 1};
         System.out.println(Arrays.stream(solution(p)));
     }
-
 
 
     public int[] solution(int[] prices) {
@@ -71,7 +77,6 @@ public class pg0903 {
             stack.push(i);
         }
 
-
         while (!stack.isEmpty()) {
             int idx = stack.pop();
             answer[idx] = prices.length - idx - 1;
@@ -79,52 +84,51 @@ public class pg0903 {
 
         return answer;
     }
+
     @Test
-    public void stack_test(){
+    public void stack_test() {
         String s = "{[]}";
         System.out.println(isValid(s));
     }
+
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack() ;
-        int n = s.length() ;
+        Stack<Character> stack = new Stack();
+        int n = s.length();
         stack.add(s.charAt(0));
-        for(int i = 1  ;i < n ;i++){
-            char check = (char)s.charAt(i);
-            if(!stack.isEmpty()){
+        for (int i = 1; i < n; i++) {
+            char check = (char) s.charAt(i);
+            if (!stack.isEmpty()) {
                 char stored = stack.peek();
-                if (check == ')'){
-                    if(stored == '('){
+                if (check == ')') {
+                    if (stored == '(') {
                         stack.pop();
-                        continue;
-                    }else{
+                    } else {
                         return false;
                     }
-                }else if(check == '}'){
-                    if(stored == '{'){
+                } else if (check == '}') {
+                    if (stored == '{') {
                         stack.pop();
-                        continue;
-                    }else{
+                    } else {
                         return false;
                     }
-                }else if(check == ']'){
-                    if(stored == '['){
+                } else if (check == ']') {
+                    if (stored == '[') {
                         stack.pop();
-                        continue;
-                    }else{
+                    } else {
                         return false;
                     }
-                }else{
+                } else {
                     stack.add(check);
                 }
-            }else{
+            } else {
                 stack.add(check);
             }
 
         }
-        if(stack.isEmpty()){
-            return true ;
+        if (stack.isEmpty()) {
+            return true;
         }
-        return false ;
+        return false;
     }
 
 }
